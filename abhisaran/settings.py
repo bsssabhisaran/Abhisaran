da21ea5d260a6400a8080def13b8e9f8b37d1e14
manan2110 <manan.co.in@gmail.com>
2021-03-06 19:20:43 +0530

    changes made

--- abhisaran/settings.py
+++ abhisaran/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'hwguz0=v87#d@sj98y!f=yf=b8e#5=j538#9a0o@l0d7=e2mbj'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['desolate-dusk-54392.herokuapp.com','abhisaran.co.in']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
