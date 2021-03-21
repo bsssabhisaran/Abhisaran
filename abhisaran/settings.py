5eeb556f11105e7aa001ba6310d2d55b83f83355
manan2110 <manan.co.in@gmail.com>
2021-03-21 14:46:56 +0530

    changes made

--- abhisaran/settings.py
+++ abhisaran/settings.py
@@ -129,5 +129,5 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'invictus.bsss@gmail.com'
-EMAIL_HOST_PASSWORD = 'bsss_21_invictus'
+EMAIL_HOST_PASSWORD = '2021_invictus@bsss'
 
