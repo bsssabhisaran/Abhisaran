2dce1572b3517800de7edbb48da18e3081cef22d
manan2110 <manan.co.in@gmail.com>
2021-06-09 08:00:38 +0530

    added gitignore and updated settings

--- abhisaran/settings.py
+++ abhisaran/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
-
+from decouple import config
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,10 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'hwguz0=v87#d@sj98y!f=yf=b8e#5=j538#9a0o@l0d7=e2mbj'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG')
 
 ALLOWED_HOSTS = ['*']
 
@@ -122,12 +122,11 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-#Email Settings
+# Email Settings
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'invictus.bsss@gmail.com'
-EMAIL_HOST_PASSWORD = '2021_invictus@bsss'
-
+EMAIL_HOST_USER = config('email')
+EMAIL_HOST_PASSWORD = config('password')
--- event/views.py
+++ event/views.py
@@ -3,11 +3,14 @@ from django.core.mail import send_mail
 
 # Create your views here.
 
+
 def home(request):
-    return render(request , 'event/index.html')
+    return render(request, 'event/index.html')
+
 
 def about(request):
-    return render(request , 'event/about.html')
+    return render(request, 'event/about.html')
+
 
 def contact(request):
     if request.method == "POST":
@@ -15,33 +18,41 @@ def contact(request):
         message_email = request.POST['email']
         message_subject = request.POST['subject']
         message = request.POST['message']
-        
-        message = message + '\n\nFrom ~ ' + message_email + '\n' + message_name
-        #send mail
-        if message_name != "" and message_email !="" and message_subject !="":
-            send_mail(message_subject , message , message_email , ['invictus.bsss@gmail.com'])
-        
-        return render(request , 'event/contact.html',{})
-       
-    return render(request , 'event/contact.html',{})
-    
+
+        message = message + '\n\nFrom :- ' + message_email + '\n' + message_name
+        # send mail
+        if message_name != "" and message_email != "" and message_subject != "":
+            send_mail(message_subject, message, message_email,
+                      ['invictus.bsss@gmail.com'])
+
+        return render(request, 'event/contact.html', {})
+
+    return render(request, 'event/contact.html', {})
+
+
 def contact_thanks(request):
-    return render(request,'event/contact2.html')
+    return render(request, 'event/contact2.html')
+
 
 def events(request):
-    return render(request , 'event/events.html',{})
+    return render(request, 'event/events.html', {})
+
 
 def cultural_events(request):
-    return render(request , 'event/cultural.html',{})
+    return render(request, 'event/cultural.html', {})
+
 
 def informal_events(request):
-    return render(request , 'event/informal.html',{})
+    return render(request, 'event/informal.html', {})
+
 
 def finearts_events(request):
-    return render(request , 'event/finearts.html',{})
+    return render(request, 'event/finearts.html', {})
+
 
 def formal_events(request):
-    return render(request , 'event/formal.html',{})
+    return render(request, 'event/formal.html', {})
+
 
 def literary_events(request):
-    return render(request , 'event/literary.html',{})
+    return render(request, 'event/literary.html', {})
